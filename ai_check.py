111b285c81adb5f3e6914495e808c9dc413cc1d9
Ryuji Kawazoe <kawazoe@val.co.jp>
2017-05-09 10:34:33 +0900

    評価を追加

--- ai_check.py
+++ ai_check.py
@@ -17,16 +17,17 @@ import pandas as pd  # dataFrameを扱うライブラリ
 # 結果算出
 from sklearn.metrics import confusion_matrix, accuracy_score, average_precision_score, f1_score, roc_auc_score
 from sklearn.metrics import recall_score, precision_score
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 
 from itertools import chain
 
 def evaluate_regression(y_train, y_train_pred):
     """
     回帰モデルの評価を行う。
-    評価関数は平均二乗誤差
+    評価関数は平均二乗誤差、決定係数
     """
-    print(mean_squared_error(y_train, y_train_pred))
+    print("平均二乗誤差", mean_squared_error(y_train, y_train_pred))
+    print("決定係数", r2_score(y_train, y_train_pred))
 
 
 
